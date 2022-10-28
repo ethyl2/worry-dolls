345bdb3a450892ef5d782f041f39cdcfe6c2cd84
ethyl2 <zzzuzz@gmail.com>
2022-10-27 19:10:20 -0600

    Feat: users now click on a doll to trigger display of form; Style: Modifies styling to use gradients in a few places

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 window.addEventListener('load', init)
 
+const listeningDollMessages = {
+  doll1: 'Please tell me your worries! I\'m listening!',
+  doll2: 'Let me know what you are worried about.',
+  doll3: 'What is causing you worry right now?',
+}
+
 function init() {
   const worriesFromSession = JSON.parse(window.sessionStorage.getItem('worries'))
   let worryArray = worriesFromSession ?? []
@@ -7,12 +13,43 @@ function init() {
   const worryInput = document.getElementById('worry')
   const reviewButton = document.getElementById('review-worries')
   const worriesList = document.getElementById('worries-list')
+  const listeningDoll = document.getElementById('listening-doll')
+  const listeningDollMessage = document.getElementById('listening-doll-message')
+  const submitWorryButton = document.getElementById('submit-worry')
+  const successMessageEl = document.getElementById('success-message')
+
+  const worryDollButtons = []
+
+  for(let i = 1; i < 4; i++) {
+      window['worryDollButton'+i] = document.getElementById(`doll${i}`)
+      worryDollButtons.push(window['worryDollButton'+i])
+  }
+
+  worryDollButtons.forEach(button => {
+    button.addEventListener('click', e => {
+      e.preventDefault()
+      worryForm.classList.remove('invisible')
+      worryForm.classList.add('visible')
+      listeningDoll.src = `./static/images/${button.id}.png`
+      listeningDollMessage.textContent = listeningDollMessages[button.id]
+    })
+  })
 
   worryForm.addEventListener('submit', (e) => {
     e.preventDefault()
     worryArray.push(worryInput.value.trim())
     window.sessionStorage.setItem('worries', JSON.stringify(worryArray))
     worryInput.value = ''
+    submitWorryButton.classList.add('hidden')
+    successMessageEl.classList.remove('hidden')
+    successMessageEl.classList.add('block')
+    setTimeout(() => {
+      console.log('here')
+      submitWorryButton.classList.remove('hidden')
+      submitWorryButton.classList.add('block')
+      successMessageEl.classList.remove('block')
+      successMessageEl.classList.add('hidden')
+    }, 1000)
   })
 
   reviewButton.addEventListener('click', () => {
