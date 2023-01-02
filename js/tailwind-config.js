d025b86bb680b4678fa3a1262cbf454874995936
Heather Nuffer <zzzuzz@gmail.com>
2023-01-02 16:38:41 -0700

    Code cleanup

--- js/tailwind-config.js
+++ js/tailwind-config.js
@@ -1,8 +1,13 @@
 tailwind.config = {
     theme: {
         extend: {
+            colors: {
+                'dark-green': '#006400',
+            },
             fontFamily: {
                 quattro: "'Quattrocento Sans', sans-serif",
+                annie: "'Annie Use Your Telescope', cursive",
+                andika: "'Andika', sans-serif"
             }
         }
     }   
